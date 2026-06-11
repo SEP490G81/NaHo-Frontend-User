5e20b29bea68a4ee13fb1f04b0300277a2f466a6
NhanPhan <ptn987654321@gmail.com>
2026-06-11 23:43:31 +0700

    #11 - feat: link public header to landing section anchors

--- layouts/public-header/types/public.header.ui.type.ts
+++ layouts/public-header/types/public.header.ui.type.ts
@@ -1,7 +1,7 @@
-import { MetadataTitleKey, StaticRoute } from "@/intl/type";
+import { AnchorRoute, MetadataTitleKey, StaticRoute } from "@/intl/type";
 
 export interface HeaderLinkItem {
     id: string;
     titleKey: MetadataTitleKey;
-    redirectLink: StaticRoute;
+    redirectLink: StaticRoute | AnchorRoute;
 }
