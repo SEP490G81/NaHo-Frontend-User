98a398682e811bfd427c123d44d8bc97a70d544a
vuongtruc2824 <vuongtrucwork2004@gmail.com>
2026-06-12 21:16:31 +0700

    fix: fix protected layout

--- app/[locale]/(protected)/layout.tsx
+++ app/[locale]/(protected)/layout.tsx
@@ -2,12 +2,9 @@ import React, { ReactNode } from "react";
 import { getCurrentUser } from "@/services/server/user.service";
 import ProtectedHeader from "@/layouts/protected-header/components/protected.header";
 import { redirect } from "next/navigation";
-import {
-    dehydrate,
-    HydrationBoundary,
-    QueryClient,
-} from "@tanstack/react-query";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import { queryKeys } from "@/libs/query.keys";
+import LearnerSidebar from "@/layouts/sidebar/components/learner.sidebar";
 
 const ProtectedLayout = async ({
     children,
@@ -25,7 +22,9 @@ const ProtectedLayout = async ({
     return (
         <HydrationBoundary state={dehydratedState}>
             <div className="relative flex">
-                <div className="w-75">Sidebar</div>
+                <div className="w-75">
+                    <LearnerSidebar />
+                </div>
                 <div className="flex min-w-0 flex-1 flex-col">
                     <ProtectedHeader />
                     <div className="bg-bgc-page w-full flex-1 p-5">
