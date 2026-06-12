15756d27a945c3131c2ddf0be24d4bc9390ed9e2
vuongtruc2824 <vuongtrucwork2004@gmail.com>
2026-06-12 21:19:08 +0700

    fix: sidebar

--- app/[locale]/(protected)/layout.tsx
+++ app/[locale]/(protected)/layout.tsx
@@ -2,6 +2,12 @@ import React, { ReactNode } from "react";
 import { getCurrentUser } from "@/services/server/user.service";
 import ProtectedHeader from "@/layouts/protected-header/components/protected.header";
 import { redirect } from "next/navigation";
+import {
+    dehydrate,
+    HydrationBoundary,
+    QueryClient,
+} from "@tanstack/react-query";
+import { queryKeys } from "@/libs/query.keys";
 import LearnerSidebar from "@/layouts/sidebar/components/learner.sidebar";
 
 const ProtectedLayout = async ({
@@ -13,16 +19,24 @@ const ProtectedLayout = async ({
         redirect("/login");
     }
 
+    const queryClient = new QueryClient();
+    queryClient.setQueryData(queryKeys.auth.currentUser, user);
+    const dehydratedState = dehydrate(queryClient);
+
     return (
-        <div className="relative flex">
-            <div className="w-75">
-                <LearnerSidebar />
+        <HydrationBoundary state={dehydratedState}>
+            <div className="relative flex">
+                <div className="w-75">
+                    <LearnerSidebar />
+                </div>
+                <div className="flex min-w-0 flex-1 flex-col">
+                    <ProtectedHeader />
+                    <div className="bg-bgc-page w-full flex-1 p-5">
+                        {children}
+                    </div>
+                </div>
             </div>
-            <div className="flex min-w-0 flex-1 flex-col">
-                <ProtectedHeader />
-                <div className="bg-bgc-page w-full flex-1 p-5">{children}</div>
-            </div>
-        </div>
+        </HydrationBoundary>
     );
 };
 
