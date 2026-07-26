93f091c2c2b42dae84aab2227965ea389268a0bb
Meocute5104 <nguyendangkhoa5104@gmail.com>
2026-07-26 18:51:27 +0900

    #33 - update: avartar effect

---
+++ hooks/use.my.subscription.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { getMySubscription } from "@/services/client/subscription.service";
+import { UserSubscriptionResponse } from "@/types/responses/subscription.response";
+
+export function useMySubscription() {
+    return useQuery<UserSubscriptionResponse | null>({
+        queryKey: ["my-subscription"],
+        queryFn: getMySubscription,
+        staleTime: 1000 * 60 * 5,
+        refetchOnWindowFocus: false,
+    });
+}
